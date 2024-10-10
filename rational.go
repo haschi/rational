310e8e770d5d6bcae8ac125f95360ca344755d00
haschi <17554782+haschi@users.noreply.github.com>
2024-10-10 12:10:11 +0000

    Basic arithmetic functions

--- rational.go
+++ rational.go
@@ -1,6 +1,9 @@
 package rational
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Rational struct {
 	numerator   int
@@ -12,7 +15,7 @@ func (r Rational) String() string {
 		return fmt.Sprint(r.numerator)
 	}
 
-	panic("Not implemented yet")
+	return fmt.Sprintf("%d/%d", r.numerator, r.denominator)
 }
 
 func FromInt(numerator int) Rational {
@@ -20,4 +23,64 @@ func FromInt(numerator int) Rational {
 		numerator:   numerator,
 		denominator: 1,
 	}
+}
+
+func New(numerator, demoniator int) (Rational, error) {
+	if demoniator == 0 {
+		return Rational{}, errors.New("denominator can not be zero")
+	}
+
+	gcd := gcd(numerator, demoniator)
+	return Rational{numerator / gcd, demoniator / gcd}, nil
+}
+
+func MustRational(a, b int) Rational {
+	rational, err := New(a, b)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return rational
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+
+	return a
+}
+
+func (left Rational) Plus(right Rational) Rational {
+	numerator := right.numerator*left.denominator + left.numerator*right.denominator
+	denominator := right.denominator * left.denominator
+	return Rational{numerator, denominator}.normalize()
+}
+
+func (left Rational) Minus(right Rational) Rational {
+	numerator := left.numerator*right.denominator - right.numerator*left.denominator
+	denominator := right.denominator * left.denominator
+	return Rational{numerator, denominator}.normalize()
+}
+
+func (left Rational) Times(right Rational) Rational {
+	numerator := left.numerator * right.numerator
+	denominator := left.denominator * right.denominator
+	return Rational{numerator, denominator}.normalize()
+}
+
+func (left Rational) DivideBy(right Rational) Rational {
+	numerator := left.numerator * right.denominator
+	denominator := left.denominator * right.numerator
+	return Rational{numerator, denominator}.normalize()
+}
+
+func (r Rational) normalize() Rational {
+	gcd := gcd(r.numerator, r.denominator)
+	result := Rational{r.numerator / gcd, r.denominator / gcd}
+	if result.denominator < 1 {
+		result = Rational{result.numerator * -1, result.denominator * -1}
+	}
+	return result
 }
--- rational_test.go
+++ rational_test.go
@@ -1,27 +1,25 @@
-package rational_test
+package rational
 
 import (
 	"fmt"
 	"testing"
-
-	"github.com/haschi/rational"
 )
 
 func TestString(t *testing.T) {
 	tests := map[string]struct {
-		number rational.Rational
+		number Rational
 		want   string
 	}{
 		"Zero": {
-			number: rational.FromInt(0),
+			number: FromInt(0),
 			want:   "0",
 		},
 		"One": {
-			number: rational.FromInt(42),
+			number: FromInt(42),
 			want:   "42",
 		},
 		"1/2": {
-			number: rational.FromInt(1),
+			number: FromInt(1),
 			want:   "1",
 		},
 	}
@@ -37,10 +35,115 @@ func TestString(t *testing.T) {
 }
 
 func TestFromInt(t *testing.T) {
-	r1 := rational.FromInt(0)
-	r2 := rational.FromInt(0)
+	r1 := FromInt(0)
+	r2 := FromInt(0)
 
 	if r1 != r2 {
-		t.Errorf("want %v == %v; got %v != %v", r1, r2, r1, r2)
+		t.Errorf("want %[1]v == %[2]v; got %[1]v != %[2]v", r1, r2)
+	}
+}
+
+func TestGcd(t *testing.T) {
+	tests := map[string]struct {
+		a    int
+		b    int
+		want int
+	}{
+		"0 gcd 1":   {0, 1, 1},
+		"1 gcd 0":   {1, 0, 1},
+		"2 gcd 1":   {2, 1, 1},
+		"-5 gcd 10": {5, 10, 5},
+		"42 gcd 11": {42, 22, 2},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := gcd(test.a, test.b)
+			if got != test.want {
+				t.Errorf("got %v; want %v", got, test.want)
+			}
+		})
+	}
+}
+
+func TestPlus(t *testing.T) {
+	tests := map[string]struct {
+		left, right, want Rational
+	}{
+		"1/2 + 1/2 = 1":   {MustRational(1, 2), MustRational(1, 2), FromInt(1)},
+		"2 + 3 = 5":       {FromInt(2), FromInt(3), FromInt(5)},
+		"1/2 + 1/3 = 5/6": {MustRational(1, 2), MustRational(1, 3), MustRational(5, 6)},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := test.left.Plus(test.right)
+			equal(t, got, test.want)
+		})
+	}
+}
+
+func TestMinus(t *testing.T) {
+	tests := map[string]struct {
+		left, right, want Rational
+	}{
+		"1/2 - 1/2 = 0":       {Rational{1, 2}, Rational{1, 2}, Rational{0, 1}},
+		"2 - 3 = -1":          {Rational{2, 1}, Rational{3, 1}, Rational{-1, 1}},
+		"1/3 - 1/5 = 2/15":    {Rational{1, 3}, Rational{1, 5}, Rational{2, 15}},
+		"-1/3 - 1/5 = -8/15":  {Rational{-1, 3}, Rational{1, 5}, Rational{-8, 15}},
+		"-1/3 - -1/5 = -2/15": {Rational{-1, 3}, Rational{-1, 5}, Rational{-2, 15}},
+		"2/3 - 1/2 = 1/6":     {Rational{2, 3}, Rational{1, 2}, Rational{1, 6}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := test.left.Minus(test.right)
+			equal(t, got, test.want)
+		})
+	}
+}
+
+func TestTimes(t *testing.T) {
+	tests := map[string]struct {
+		left, right, want Rational
+	}{
+		"1/2 * 1/2 = 1/4":     {Rational{1, 2}, Rational{1, 2}, Rational{1, 4}},
+		"2 * 3 = 6":           {Rational{2, 1}, Rational{3, 1}, Rational{6, 1}},
+		"1/3 * 1/5 = 1/15":    {Rational{1, 3}, Rational{1, 5}, Rational{1, 15}},
+		"-1/3 * -1/5 = -1/15": {Rational{-1, 3}, Rational{1, 5}, Rational{-1, 15}},
+		"1/3 * -1/5 = -1/15":  {Rational{1, 3}, Rational{-1, 5}, Rational{-1, 15}},
+		"1/2 * 3/4 = 3/8":     {Rational{1, 2}, Rational{3, 4}, Rational{3, 8}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := test.left.Times(test.right)
+			equal(t, got, test.want)
+		})
+	}
+}
+
+func TestDivideBy(t *testing.T) {
+	tests := map[string]struct {
+		left, right, want Rational
+	}{
+		"8 / 2 = 4":         {FromInt(8), FromInt(2), FromInt(4)},
+		"2/3 / 4/5 = 5/6":   {Rational{2, 3}, Rational{4, 5}, Rational{5, 6}},
+		"-2/3 / 4/5 = -5/6": {Rational{-2, 3}, Rational{4, 5}, Rational{-5, 6}},
+		"2/3 / -4/5 = 5/6":  {Rational{2, 3}, Rational{-4, 5}, Rational{-5, 6}},
+		"1/2 / 3/4 = 2/3":   {Rational{1, 2}, Rational{3, 4}, Rational{2, 3}},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := test.left.DivideBy(test.right)
+			equal(t, got, test.want)
+		})
+	}
+}
+
+func equal[T comparable](t *testing.T, got, want T) {
+	if got != want {
+		t.Errorf("got %v; want %v", got, want)
 	}
 }
