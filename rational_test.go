8b9e3b699a4aadf39c86488fb282cb3eedf89c84
haschi <17554782+haschi@users.noreply.github.com>
2024-10-10 17:00:01 +0000

    Fixed strings with rational numbers for testing

--- rational_test.go
+++ rational_test.go
@@ -18,7 +18,7 @@ func TestString(t *testing.T) {
 			number: FromInt(42),
 			want:   "42",
 		},
-		"1/2": {
+		"1\u22152": {
 			number: FromInt(1),
 			want:   "1",
 		},
@@ -31,9 +31,7 @@ func TestString(t *testing.T) {
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			got := fmt.Sprint(test.number)
-			if got != test.want {
-				t.Errorf("want %v; got %v", test.want, got)
-			}
+			equal(t, got, test.want)
 		})
 	}
 }
@@ -74,12 +72,12 @@ func TestPlus(t *testing.T) {
 	tests := map[string]struct {
 		left, right, want Rational
 	}{
-		"1/2 + 1/2 = 1":   {Rational{1, 2}, Rational{1, 2}, FromInt(1)},
-		"2 + 3 = 5":       {FromInt(2), FromInt(3), FromInt(5)},
-		"1/2 + 1/3 = 5/6": {Rational{1, 2}, Rational{1, 3}, Rational{5, 6}},
-		"NaN + 1/2 = NaN": {Rational{1, 0}, Rational{1, 2}, Rational{}},
-		"1/2 + NaN = NaN": {Rational{1, 0}, Rational{1, 2}, Rational{}},
-		"NaN + NaN = NaN": {Rational{}, Rational{}, Rational{}},
+		"1\u22152 + 1\u22152 = 1":        {Rational{1, 2}, Rational{1, 2}, FromInt(1)},
+		"2 + 3 = 5":                      {FromInt(2), FromInt(3), FromInt(5)},
+		"1\u22152 + 1\u22153 = 5\u22156": {Rational{1, 2}, Rational{1, 3}, Rational{5, 6}},
+		"NaN + 1\u22152 = NaN":           {Rational{1, 0}, Rational{1, 2}, Rational{}},
+		"1\u22152 + NaN = NaN":           {Rational{1, 0}, Rational{1, 2}, Rational{}},
+		"NaN + NaN = NaN":                {Rational{}, Rational{}, Rational{}},
 	}
 
 	for name, test := range tests {
@@ -94,15 +92,15 @@ func TestMinus(t *testing.T) {
 	tests := map[string]struct {
 		left, right, want Rational
 	}{
-		"1/2 - 1/2 = 0":       {Rational{1, 2}, Rational{1, 2}, Rational{0, 1}},
-		"2 - 3 = -1":          {Rational{2, 1}, Rational{3, 1}, Rational{-1, 1}},
-		"1/3 - 1/5 = 2/15":    {Rational{1, 3}, Rational{1, 5}, Rational{2, 15}},
-		"-1/3 - 1/5 = -8/15":  {Rational{-1, 3}, Rational{1, 5}, Rational{-8, 15}},
-		"-1/3 - -1/5 = -2/15": {Rational{-1, 3}, Rational{-1, 5}, Rational{-2, 15}},
-		"2/3 - 1/2 = 1/6":     {Rational{2, 3}, Rational{1, 2}, Rational{1, 6}},
-		"NaN - 1/2 = NaN":     {Rational{1, 0}, Rational{1, 2}, Rational{}},
-		"1/2 - NaN = Nan":     {Rational{1, 2}, Rational{42, 0}, Rational{}},
-		"NaN - NaN = NaN":     {Rational{5, 0}, Rational{7, 0}, Rational{}},
+		"1\u22152 - 1\u22152 = 0":            {Rational{1, 2}, Rational{1, 2}, Rational{0, 1}},
+		"2 - 3 = -1":                         {Rational{2, 1}, Rational{3, 1}, Rational{-1, 1}},
+		"1\u22153 - 1\u22155 = 2\u221515":    {Rational{1, 3}, Rational{1, 5}, Rational{2, 15}},
+		"-1\u22153 - 1\u22155 = -8\u221515":  {Rational{-1, 3}, Rational{1, 5}, Rational{-8, 15}},
+		"-1\u22153 - -1\u22155 = -2\u221515": {Rational{-1, 3}, Rational{-1, 5}, Rational{-2, 15}},
+		"2\u22153 - 1\u22152 = 1\u22156":     {Rational{2, 3}, Rational{1, 2}, Rational{1, 6}},
+		"NaN - 1\u22152 = NaN":               {Rational{1, 0}, Rational{1, 2}, Rational{}},
+		"1\u22152 - NaN = Nan":               {Rational{1, 2}, Rational{42, 0}, Rational{}},
+		"NaN - NaN = NaN":                    {Rational{5, 0}, Rational{7, 0}, Rational{}},
 	}
 
 	for name, test := range tests {
@@ -117,14 +115,14 @@ func TestTimes(t *testing.T) {
 	tests := map[string]struct {
 		left, right, want Rational
 	}{
-		"1/2 * 1/2 = 1/4":     {Rational{1, 2}, Rational{1, 2}, Rational{1, 4}},
-		"2 * 3 = 6":           {Rational{2, 1}, Rational{3, 1}, Rational{6, 1}},
-		"1/3 * 1/5 = 1/15":    {Rational{1, 3}, Rational{1, 5}, Rational{1, 15}},
-		"-1/3 * -1/5 = -1/15": {Rational{-1, 3}, Rational{1, 5}, Rational{-1, 15}},
-		"1/3 * -1/5 = -1/15":  {Rational{1, 3}, Rational{-1, 5}, Rational{-1, 15}},
-		"1/2 * 3/4 = 3/8":     {Rational{1, 2}, Rational{3, 4}, Rational{3, 8}},
-		"NaN * 1/2 = NaN":     {Rational{}, Rational{1, 2}, Rational{}},
-		"1/2 * NaN = NaN":     {Rational{1, 2}, Rational{47, 0}, Rational{}},
+		"1\u22152 * 1\u22152 = 1\u22154":     {Rational{1, 2}, Rational{1, 2}, Rational{1, 4}},
+		"2 * 3 = 6":                          {Rational{2, 1}, Rational{3, 1}, Rational{6, 1}},
+		"1\u22153 * 1\u22155 = 1\u221515":    {Rational{1, 3}, Rational{1, 5}, Rational{1, 15}},
+		"-1\u22153 * -1\u22155 = -1\u221515": {Rational{-1, 3}, Rational{1, 5}, Rational{-1, 15}},
+		"1\u22153 * -1\u22155 = -1\u221515":  {Rational{1, 3}, Rational{-1, 5}, Rational{-1, 15}},
+		"1\u22152 * 3\u22154 = 3\u22158":     {Rational{1, 2}, Rational{3, 4}, Rational{3, 8}},
+		"NaN * 1\u22152 = NaN":               {Rational{}, Rational{1, 2}, Rational{}},
+		"1\u22152 * NaN = NaN":               {Rational{1, 2}, Rational{47, 0}, Rational{}},
 	}
 
 	for name, test := range tests {
@@ -139,13 +137,13 @@ func TestDivideBy(t *testing.T) {
 	tests := map[string]struct {
 		left, right, want Rational
 	}{
-		"8 / 2 = 4":         {FromInt(8), FromInt(2), FromInt(4)},
-		"2/3 / 4/5 = 5/6":   {Rational{2, 3}, Rational{4, 5}, Rational{5, 6}},
-		"-2/3 / 4/5 = -5/6": {Rational{-2, 3}, Rational{4, 5}, Rational{-5, 6}},
-		"2/3 / -4/5 = 5/6":  {Rational{2, 3}, Rational{-4, 5}, Rational{-5, 6}},
-		"1/2 / 3/4 = 2/3":   {Rational{1, 2}, Rational{3, 4}, Rational{2, 3}},
-		"1/2 / NaN = NaN":   {Rational{1, 2}, Rational{}, Rational{}},
-		"NaN / 1/2 = NaN":   {Rational{}, Rational{1, 2}, Rational{}},
+		"8 \u2215 2 = 4":                        {FromInt(8), FromInt(2), FromInt(4)},
+		"2\u22153 \u2215 4\u22155 = 5\u22156":   {Rational{2, 3}, Rational{4, 5}, Rational{5, 6}},
+		"-2\u22153 \u2215 4\u22155 = -5\u22156": {Rational{-2, 3}, Rational{4, 5}, Rational{-5, 6}},
+		"2\u22153 \u2215 -4\u22155 = 5\u22156":  {Rational{2, 3}, Rational{-4, 5}, Rational{-5, 6}},
+		"1\u22152 \u2215 3\u22154 = 2\u22153":   {Rational{1, 2}, Rational{3, 4}, Rational{2, 3}},
+		"1\u22152 \u2215 NaN = NaN":             {Rational{1, 2}, Rational{}, Rational{}},
+		"NaN \u2215 1\u22152 = NaN":             {Rational{}, Rational{1, 2}, Rational{}},
 	}
 
 	for name, test := range tests {
@@ -161,10 +159,10 @@ func TestIsNaN(t *testing.T) {
 		r    Rational
 		want bool
 	}{
-		"0/0":  {Rational{0, 0}, true},
-		"1/0":  {Rational{1, 0}, true},
-		"-1/0": {Rational{-1, 0}, true},
-		"0/1":  {Rational{0, 1}, false},
+		"0\u22150":  {Rational{0, 0}, true},
+		"1\u22150":  {Rational{1, 0}, true},
+		"-1\u22150": {Rational{-1, 0}, true},
+		"0\u22151":  {Rational{0, 1}, false},
 	}
 
 	for name, test := range tests {
